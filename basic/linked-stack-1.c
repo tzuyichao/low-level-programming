55919ec7d29be481942eaeb8c0c16732a668121f
TzuYi Chao <tzuyichao0313@gmail.com>
2022-03-16 11:04:21 +0800

    linked stack

---
+++ basic/linked-stack-1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct items {
+  int data;
+  struct items *link;
+} ITEM;
+
+typedef struct stack {
+    ITEM *top;
+} STACK;
+
+void initStack(STACK *s) {
+    s->top = NULL;
+}
+
+void pushStack(STACK *s, int y) {
+    ITEM *x;
+    x = (ITEM *)malloc(sizeof(ITEM));
+    x->data = y;
+    x->link = s->top;
+    s->top = x;
+}
+
+int popStack(STACK *s) {
+    ITEM * x = s->top;
+    int d = x->data;
+    s->top = s->top->link;
+    free(x);
+    return d;
+}
+
+int stackIsEmpty(STACK *s) {
+    return s->top == NULL;
+}
+
+int main() {
+    STACK s;
+    int i;
+    initStack(&s);
+    for(int i=0; i<10; i++) {
+        pushStack(&s, i);
+    }
+    while(!stackIsEmpty(&s)) {
+        printf("%d\n", popStack(&s));
+    }
+}
