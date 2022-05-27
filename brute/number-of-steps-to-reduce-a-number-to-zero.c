b3e253b5f1d59a9b6ac592c25d8af6c589959adf
TzuYi Chao <tzuyichao0313@gmail.com>
2022-05-27 08:19:54 +0800

    Number of Steps to Reduce a Number to Zero

--- brute/number-of-steps-to-reduce-a-number-to-zero.c
+++ brute/number-of-steps-to-reduce-a-number-to-zero.c
@@ -1,14 +1,15 @@
 // 1342. Number of Steps to Reduce a Number to Zero
 // https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
-int numberOfSteps(int num){
+int numberOfSteps(int num) {
     int res = 0;
     
     while(num > 0) {
-        if(num % 2 == 1) {
-            --num;
-        } else {
-            num >>= 1;
-        }
+        // if(num % 2 == 1) {
+        //     --num;
+        // } else {
+        //     num >>= 1;
+        // }
+        num = num%2==1?--num:num>>1;
         ++res;
     }
     
