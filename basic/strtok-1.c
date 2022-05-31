786ab853c2d1f6c73db8334f2b22076c9cdb7546
TzuYi Chao <tzuyichao0313@gmail.com>
2022-05-31 08:42:46 +0800

    hacker ranker prepare

---
+++ basic/strtok-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "Hello world, nice to meet you";
+    char str2[64];
+    strcpy(str2, str);
+
+    const char *d = " ,";
+    char *p;
+
+    p = strtok(str, d);
+    while(p != NULL) {
+        printf("%s\n", p);
+        p = strtok(NULL, d);
+    }
+
+    printf("----\n");
+    printf("%s\n", str);
+    printf("%s\n", str2);
+    
+    return 0;
+}
