0ddd3a4636a4d95095948623d9c95b923a17f377
TzuYi Chao <tzuyichao0313@gmail.com>
2022-06-09 08:45:42 +0800

    two pointer

--- brute/two-sum-II-input-array-is-sorted.c
+++ brute/two-sum-II-input-array-is-sorted.c
@@ -8,18 +8,18 @@ int* twoSum(int* numbers, int numbersSize, int target, int* returnSize)
 {
     int* res = (int*)malloc(2 * sizeof(int));
 
-    for(int i=0; i<numbersSize; i++) {
-        for(int j=0; j<numbersSize; j++) {
-            if(i != j && numbers[i] + numbers[j] == target) {
-                if(i>j) {
-                    res[0] = j+1;
-                    res[1] = i+1;
-                } else {
-                    res[0] = i+1;
-                    res[1] = j+1;
-                }
-                break;
-            } 
+    int i = 0;
+    int j = numbersSize-1;
+    while(i < j) {
+        int sum = numbers[i] + numbers[j];
+        if(sum == target) {
+            res[0] = i+1;
+            res[1] = j+1;
+            break;
+        } else if(sum > target) {
+            --j;
+        } else {
+            ++i;
         }
     }
     *returnSize = 2;
