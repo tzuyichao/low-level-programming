082e9537d35402755ed4784dea150cabcd60163a
TzuYi Chao <tzuyichao0313@gmail.com>
2022-06-17 16:29:22 +0800

    ch2

---
+++ linux-system/my-sys.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/sysinfo.h>
+
+int main(void)
+{
+    char cwd[100] = { 0 };
+    struct sysinfo si;
+    getcwd(cwd, 100);
+    sysinfo(&si);
+
+    printf("Your user ID is %d\n", getuid());
+    printf("Your effective user ID is %d\n", geteuid());
+    printf("Your current working directory is %s\n", cwd);
+    printf("Your machine has %ld megabytes of total RAM\n", si.totalram/1024/1024);
+    printf("Your machine has %ld megabytes of free RAM\n", si.freeram/1024/1024);
+    printf("Currently, there are %d processes running\n", si.procs);
+    printf("The process ID is %d\n", getpid());
+    printf("The parent process ID is %d\n", getppid());
+
+    return 0;
+}
