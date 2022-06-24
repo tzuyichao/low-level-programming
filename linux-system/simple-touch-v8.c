6eca62d29ca4cbe1a5027c2f2e1a1a14c0e0f4e9
TzuYi Chao <tzuyichao0313@gmail.com>
2022-06-24 09:25:48 +0800

    update files timestamp or create one

---
+++ linux-system/simple-touch-v8.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <utime.h>
+#define MAX_LENGTH 100
+
+int main(int argc, char *argv[])
+{
+    char filename[MAX_LENGTH] = {0};
+    if(argc != 2)
+    {
+        fprintf(stderr, "You must supply a filename");
+        return 1;
+    }
+    strncat(filename, argv[1], sizeof(filename)-1);
+    if(utime(filename, NULL) == -1)
+    {
+        if(errno == ENOENT)
+        {
+            if(creat(filename, 00644) == -1)
+            {
+                perror("Can't create file");
+                return errno;
+            }
+        }
+        else
+        {
+            perror("Can't update timestamp");
+            return errno;
+        }
+    }
+    return 0;
+}
