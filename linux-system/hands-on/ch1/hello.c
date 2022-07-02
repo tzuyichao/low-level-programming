2da1b37af4f0f05be666b09d8149481c517fd582
TzuYi Chao <tzuyichao0313@gmail.com>
2022-07-02 22:47:57 +0800

    chapter 1 hello example

---
+++ linux-system/hands-on/ch1/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a;
+    printf("Hello, Linux System Programming, World!\n");
+    a = 5;
+
+    return 0;
+}
