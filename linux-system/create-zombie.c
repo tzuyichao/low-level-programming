87770a8a33ed8836d6d5fbeb5209a54ce212a04b
tzuyichao <tzuyichao0313@gmail.com>
2022-08-17 17:04:00 +0800

    creating a zombie process

---
+++ linux-system/create-zombie.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	pid_t pid;
+	printf("My PID is %d\n", getpid());
+	if((pid = fork()) == -1)
+	{
+		perror("Can't fork");
+		return 1;
+	}
+	if(pid == 0)
+	{
+		printf("Hello and goodbye from the child! %d\n", getpid());
+		exit(0);
+	}
+	else if(pid > 0)
+	{
+		printf("Hello from the parent process! My child had PID %d\n", pid);
+		sleep(120);
+	}
+	else
+	{
+		fprintf(stderr, "Something went wrong forking!\n");
+		return 1;
+	}
+	return 0;
+}
